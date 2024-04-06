87fb127fbef8fe8e155066ee20418b30a5f38276
fifki <f.ifkirne21@gmail.com>
2024-04-06 03:01:47 +0200

    the coin change with tabulation

--- dynamic_programming/coin_change.c
+++ dynamic_programming/coin_change.c
@@ -2,15 +2,9 @@
 
 //dynamic programming tabulation
 
-int min(int size , int tab[size]){
-    int mini= tab[0];
-    for (int i = 1; i < size; i++)
-    {
-        if( mini>tab[i]){
-            mini=tab[i];
-        }
-    }
-    return mini;
+int min(int a ,  int b){
+    if(a<b){return a; }
+    else {return b; }
 }
 int coin_change(int m, int nb, int coin_tab[nb]);
 int coin_change(int m, int nb, int coin_tab[nb]){
@@ -22,20 +16,24 @@ for (int i=1; i<m+1;i++ ){
     resultat[i] = -1;
     for (int j = 0; j < nb; j++)
     {
-        if (i - coin_tab[j] >= 0 && resultat[i-coin_tab[j]]!=-1) { 
+        if(i - coin_tab[j] >= 0 && resultat[i]!= -1 && resultat[i-coin_tab[j]]!= -1){
+            resultat[i]=min(resultat[i] , 1 + resultat[i-coin_tab[j]]);
+        }
+        else if(i - coin_tab[j] >= 0 && resultat[i]== -1 && resultat[i-coin_tab[j]]!= -1){
+            resultat[i]= 1 + resultat[i-coin_tab[j]];
+        }
+        
 
-         resultat[i]=min(2, (int[]){resultat[i] , 1+ resultat[i-coin_tab[j]]});
-        }
-        }
+    }
 }
  return resultat[m]; 
 }
 int main(){
-int money=15;
-int nb=3;
-int possible_coin[3]={2,3,7};
-int nb_coin;
-nb_coin=coin_change(money, nb ,possible_coin );
-printf(" the nb_coin are %d\r\n", nb_coin);
-    return 0;
+    int money=80;
+    int nb=3;
+    int possible_coin[3]={1,20,25};
+    int nb_coin;
+    nb_coin=coin_change(money, nb ,possible_coin );
+    printf(" the nb_coin are %d\r\n", nb_coin);
+        return 0;
 }
