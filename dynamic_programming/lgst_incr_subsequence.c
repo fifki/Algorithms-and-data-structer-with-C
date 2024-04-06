592a28c93fa48f2bf7983025aa5402cdf0fd82b5
fifki <f.ifkirne21@gmail.com>
2024-04-06 18:40:54 +0200

    new problem

---
+++ dynamic_programming/lgst_incr_subsequence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int longest (int nb , int array[nb]){
+
+
+
+
+}
+int main(){
+int nb ; 
+int array[13]={7, 5, 2, 4, 7, 2, 3, 6, 4, 5, 12, 1, 7};
+int resultat= longest(nb , array);
+printf("%d\r\n", resultat);
+    return 0;
+}
