d373a9bf4a23e2a81bdc6080896dee9ccc0adfd8
fifki <f.ifkirne21@gmail.com>
2024-04-06 18:11:07 +0200

    update

--- dynamic_programming/subset_sum.c
+++ dynamic_programming/subset_sum.c
@@ -1,11 +1,62 @@
 #include<stdio.h>
-int subset_sum(int k, int nb , int array[nb]){
 
+/*
+Subset sum (problem)
+Given an array arr of strictly positive integers,
+ and an integer k, create a function that returns 
+ the number of subsets of arr that sum up to k.
+Example 1:
+input:
+arr = [1, 2, 3, 1]
+k = 4
+output: 3
+explanation: subsets that sum up to k are [1, 2, 1], [1, 3], 
+and [3, 1]*/
+int subset_sum(int k, int nb , int array[nb]){
+    int resultat[nb][k+1];
+    int somme=0;
+    for(int i=0; i<nb; i++){
+    somme+=array[i];
+    }
+
+    if(k>somme || nb==0 ){return 0;}
+
+    else{
+        
+        resultat[0][0]=1;
+        if(array[0]<=k){
+            resultat[0][array[0]]=1;
+        }
+
+        for (int i = 1; i < nb; i++)
+        {
+            for (int j = 0; j < k+1; j++)
+            {
+                if(j-array[i]>=0){
+                    resultat[i][j]=resultat[i-1][j]+resultat[i-1][j-array[i]];
+                }
+                else{
+                    resultat[i][j]=resultat[i-1][j];
+                }
+            }
+        } 
+        }
+    
+    //debug:
+        for (int i = 0; i < nb; i++)
+    {
+        printf( " \r\n----------------------------\r\n");
+        for (int j = 0; j < k+1; j++)
+        {
+            printf(" | %d ", resultat[i][j]);
+        } 
+    }
+    return resultat[nb-1][k];
 }
 int main(){
-    int nb=4, k=4;
-    int array[4]={1,2,3,1};
+    int nb=5, k=5;
+    int array[5]={1,2,3,1,4};
     int result=subset_sum(k, nb, array);
-    printf("%d\r\n", result);
+    printf("\r\n %d\r\n", result);
     return 0;
 }
