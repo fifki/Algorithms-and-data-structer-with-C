300cd5ca5e3dd9f1f021a13997bbefffa2779b95
fifki <f.ifkirne21@gmail.com>
2024-04-06 07:48:14 +0200

    new problem

---
+++ dynamic_programming/subset_sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int subset_sum(int k, int nb , int array[nb]){
+
+}
+int main(){
+    int nb=4, k=4;
+    int array[4]={1,2,3,1};
+    int result=subset_sum(k, nb, array);
+    printf("%d\r\n", result);
+    return 0;
+}
