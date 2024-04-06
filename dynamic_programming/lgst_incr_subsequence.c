c1124a8ea6cc15a2b6e8c0bcf752d29532a1472c
fifki <f.ifkirne21@gmail.com>
2024-04-06 19:28:11 +0200

    yupii

--- dynamic_programming/lgst_incr_subsequence.c
+++ dynamic_programming/lgst_incr_subsequence.c
@@ -1,27 +1,46 @@
 #include<stdio.h>
-int longest (int nb , int array[nb]){
-int resultat[nb];
-int somme=0;
-int old=0, new=0;
-for (int i = 0; i < nb; i++)
-{
-    somme=array[i];
-    for (int j = i ; i < nb ; j++)
+int maxi(int size , int tab[size]){
+    int max= tab[0];
+    for (int i = 1; i < size; i++)
     {
-        if(array[j+1]>array[j]){
-        somme+=array[j+1];}
-        else {
-            continue;
+        if(max<tab[i]){
+            max=tab[i];
         }
     }
-    
+    return max;
 }
+
+int longest (int nb , int array[nb]){
+int resultat[nb];
+resultat[0]=1;
+int max=0;
+for (int i = 1; i < nb; i++)
+{
+    max=0;
+    for (int j = 0 ; j < i ; j++)
+    {
+        if(array[j]< array[i]&& resultat[j]>max){
+        max=resultat[j];
+        }
+       
+    } 
+    resultat[i]=1+max;
+   
+}
+/*/
+    printf( " \r\n----------------------------\r\n");
+    for (int i = 0; i < nb; i++)
+    {
+        printf(" | %d ", resultat[i]);
+    }*/ 
+
+    return maxi(nb , resultat);
 
 }
 int main(){
-int nb ; 
+int nb=13 ; 
 int array[13]={7, 5, 2, 4, 7, 2, 3, 6, 4, 5, 12, 1, 7};
 int resultat= longest(nb , array);
-printf("%d\r\n", resultat);
+printf("\r\n%d\r\n", resultat);
     return 0;
 }
