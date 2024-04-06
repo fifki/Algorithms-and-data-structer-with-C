ea8b5b00fed19b4773e473e6396ffbefb9ade98d
fifki <f.ifkirne21@gmail.com>
2024-04-06 18:54:45 +0200

    nothing

--- dynamic_programming/lgst_incr_subsequence.c
+++ dynamic_programming/lgst_incr_subsequence.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 int longest (int nb , int array[nb]){
+int resultat[nb];
+int somme=0;
+int old=0, new=0;
+for (int i = 0; i < nb; i++)
+{
+    somme=array[i];
+    for (int j = i ; i < nb ; j++)
 
-
-
+    {
+        if(array[j+1]>array[j]){
+        somme+=array[j+1];}
+        else 
+    }
+    
+}
 
 }
 int main(){
