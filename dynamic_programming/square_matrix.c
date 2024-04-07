e26a9c065a4a0da2d57c102a2e41de9c1db491e1
fifki <f.ifkirne21@gmail.com>
2024-04-07 02:39:47 +0200

    final file

--- dynamic_programming/square_matrix.c
+++ dynamic_programming/square_matrix.c
@@ -19,14 +19,65 @@ matrix = [
 
 output: 9
 */
-int square(int n, int m , int matrix[n][m]){
-
-
-
-
-
+int min(int size , int tab[size]){
+    int mini= tab[0];
+    for (int i = 1; i < size; i++)
+    {
+        if(mini>tab[i]){
+            mini=tab[i];
+        }
+    }
+return mini;
+}
+int max(int n ,int m, int matrix[n][m]){
+    int maxi = matrix[0][0];
+    for (int i = 1; i <n; i++)
+    {
+        for(int j =1; j<m;j++){
+            if(matrix[i][j]>maxi ){maxi= matrix[i][j];}
+        }
+}
+return maxi;
 }
 
+int square(int n, int m , int matrix[n][m]){
+int resultat[n][m];
+//base case
+resultat[0][0]=matrix[0][0];
+for (int j = 1; j < m; j++)
+{
+    resultat[0][j]=matrix[0][j];
+}
+for (int j = 1; j < n; j++)
+{
+    resultat[j][0]=matrix[j][0];
+}
+//otherwise
+for (int i = 1; i <n; i++)
+{
+    for(int j =1; j<m;j++){
+        if(matrix[i][j]==0 ){resultat[i][j]=0;}
+        else{
+            resultat[i][j]=1+min(3, (int[]){resultat[i-1][j], resultat[i][j-1], resultat[i-1][j-1]});
+        }
+    }
+}
+//to debug:
+/*/
+      for (int i = 0; i < n; i++)
+    {
+        printf( " \r\n----------------------------\r\n");
+        for (int j = 0; j < m; j++)
+        {
+            printf(" | %d ", resultat[i][j]);
+        } 
+    } */
+int max_value = max(n, m, resultat);
+return max_value * max_value;
+}
+
+//test  
+/**
 int main(){
 int n=5, m=6;
 int matrix[5][6] = {
@@ -40,8 +91,8 @@ int matrix[5][6] = {
     printf("\r\n %d", resultat);
     return 0;
 }
+*/
 
-/**
 int main(){
     int n, m;
     printf("what is n ?\r\n");
@@ -56,7 +107,7 @@ int main(){
         scanf("%d", &matrix[i][j]);
         }
     }
-    int resultat= gold_mine(n , m , matrix);
+    int resultat= square(n , m , matrix);
     printf("\r\n %d", resultat);
     return 0;
-}*/
+}
